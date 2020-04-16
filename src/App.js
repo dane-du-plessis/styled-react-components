d849a27b54ce4ab7105729a72d924e9519535f04
Dane du Plessis <daneasc@gmail.com>
2020-04-16 14:36:07 +0200

    Made a radio button and styled it. Muuuuuch faster than doing it with the full project running.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import keyframes from "@emotion/core";
+import styled from "@emotion/styled";
 
 function App() {
+  const [select=false, setSelect] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container
+        onClick={() => {
+          console.log(select);
+          console.log("Clicked!");
+          setSelect(!select);
+          console.log(select);
+        }}
+      >
+        <Circle selected={select}/>
+        <p>Some label</p>
+      </Container>
+    </>
   );
 }
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Circle = styled.div`
+  width: 22px;
+  height: 22px;
+  border-radius: 50%;
+  background-color: transparent;
+  border-color: gray;
+  border-style: solid;
+  border-width: 2px;
+  position: relative;
+  transition-duration: 1s;
+  &:after {
+    content: ' ';
+    position: absolute;
+    top: 5px;
+    left: 5px;
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    background-color: blue;
+  }
+
+  ${(p) =>
+    p.selected
+      ? `&:after {
+        width: 12px;
+        height: 12px;
+        top: 5px;
+        left: 5px;
+        background-color: blue;
+        transition: all 0.2s;
+      }`
+      : `&:after {
+        width: 0px;
+        height: 0px;
+        top: 11px;
+        left: 11px;
+        background-color: transparent;
+        transition: all 0.2s;
+      }`}
+
+  &:hover {
+    background-color: rgba(0, 0,255,0.5);
+    transition: 1s;
+  }
+
+  &:focus {
+    // todo style
+    outline: none;
+  }
+`;
+
+export const Label = styled.div`
+  font-size: 16px;
+  color: black;
+  padding-left: 8px;
+  user-select: none;
+`;
 
 export default App;
