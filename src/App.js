9c10e2bb5c714f10ec0c9b2954874dc65c7169d1
Dane du Plessis <daneasc@gmail.com>
2020-04-20 21:26:50 +0200

    Built a dropown.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-import React, { useState } from "react";
-import keyframes from "@emotion/core";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "@emotion/styled";
-
+// https://medium.com/@pitipatdop/little-neat-trick-to-capture-click-outside-with-react-hook-ba77c37c7e82
 function App() {
-  const [select=false, setSelect] = useState(0);
+  const node = useRef();
+  const [select = false, setSelect] = useState("All");
+  const [expanded = false, setExpanded] = useState(false);
+  const options = ["One", "Two", "Three"];
+
+  useEffect(() => {
+    // add when mounted
+    if (expanded) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+    // return function to be called when unmounted
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [expanded]);
+
+  const handleClickOutside = (e) => {
+    console.log("clicking anywhere");
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
+    }
+    // outside click
+    setExpanded(false);
+  };
+
+  const handleSelect = (index) => {
+    console.log("select!");
+    setSelect(options[index]);
+    setExpanded(false);
+  };
+
+  const expandOptions = () => {
+    setExpanded(!expanded);
+    console.log(expanded);
+  };
 
   return (
     <>
-      <Container
-        onClick={() => {
-          console.log(select);
-          console.log("Clicked!");
-          setSelect(!select);
-          console.log(select);
-        }}
-      >
-        <Circle selected={select}/>
-        <p>Some label</p>
+      <Container ref={node} onClick={() => {}}>
+        <Selected onClick={expandOptions}>
+          <p>{select}</p>
+        </Selected>
+
+        {expanded
+          ? options.map((op, index) => (
+              <Options onClick={(e) => handleSelect(index)}>
+                <p>{op}</p>
+              </Options>
+            ))
+          : null}
+      </Container>
+      <Container>
+        <p>Some other part of doc</p>
       </Container>
     </>
   );
@@ -24,65 +65,26 @@ function App() {
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  width: auto;
   align-items: center;
+  background-color: #aaaaaa;
+  padding: 2px;
 `;
 
-export const Circle = styled.div`
-  width: 22px;
-  height: 22px;
-  border-radius: 50%;
-  background-color: transparent;
-  border-color: gray;
-  border-style: solid;
-  border-width: 2px;
-  position: relative;
-  transition-duration: 1s;
-  &:after {
-    content: ' ';
-    position: absolute;
-    top: 5px;
-    left: 5px;
-    width: 12px;
-    height: 12px;
-    border-radius: 50%;
-    background-color: blue;
-  }
+export const Selected = styled.div`
+  background-color: #cccccc;
+  width: stretch;
+  padding: 4px;
+`;
 
-  ${(p) =>
-    p.selected
-      ? `&:after {
-        width: 12px;
-        height: 12px;
-        top: 5px;
-        left: 5px;
-        background-color: blue;
-        transition: all 0.2s;
-      }`
-      : `&:after {
-        width: 0px;
-        height: 0px;
-        top: 11px;
-        left: 11px;
-        background-color: transparent;
-        transition: all 0.2s;
-      }`}
-
+export const Options = styled.div`
+  width: stretch;
+  border-radius: 8px;
+  padding: 4px;
   &:hover {
-    background-color: rgba(0, 0,255,0.5);
-    transition: 1s;
+    background-color: red;
   }
-
-  &:focus {
-    // todo style
-    outline: none;
-  }
-`;
-
-export const Label = styled.div`
-  font-size: 16px;
-  color: black;
-  padding-left: 8px;
-  user-select: none;
 `;
 
 export default App;
